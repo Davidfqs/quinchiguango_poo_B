bc33e3b5e1dbe1c2e50aa001c69108cbd0eeb4fd
David <David@David>
2021-06-30 16:54:53 -0500

    Primera version del proyecto de Programacion Orientada a Objetos paralelo B

---
+++ src/ec/edu/intsuperior/modelo/Linea.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author David
+ */
+public class Linea {
+    private Punto p1;
+    private Punto p2;
+    
+}
